106617e09c42638e86428edc7d41f4fe27356362
ArunWadhwagit <74895789+ArunWadhwagit@users.noreply.github.com>
2022-03-29 23:44:44 +0530

    myfirstcommit

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  subscription_id = "27484c4c-7a53-4889-a51c-dde9afa85dfa"
+  client_id = "349b09a0-3e18-46dc-b7fd-e44d52af4e2a"
+  client_secret = "AZ0XhVPiGdi9TfE3w8Nr.frSjD2MegUb.P"
+  tenant_id = "eb333f83-c9ea-4009-b18b-b55d415ee1f7"
+  features {
+    
+  }
+}
+resource "azurerm_resource_group" "RG-30march" {
+  name     = "RG-30march"
+  location = "eastus"
+}
+resource "azurerm_resource_group" "RG-31march" {
+  name     = "RG-31march"
+  location = "eastus"
+}
